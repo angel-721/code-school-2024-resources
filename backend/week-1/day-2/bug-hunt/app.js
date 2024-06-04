3f930b6f98dd9c5fa1cada05b3d2be9af8ce8a18
angel-721 <angelvelasquez2002@gmail.com>
2024-06-03 23:21:33 -0600

    Bug hunt!

---
+++ backend/week-1/day-2/bug-hunt/app.js
@@ -0,0 +1,14 @@
+let hi_button = document.getElementById("#hi-button");
+
+hi_button.onclick = () = {
+	// I was too lazy to finish this code
+}
+
+
+let edit_distance_h2 = document.getElementById("edit-distance");
+edit_distance_h2 = "Edit Distance";
+
+
+let sum = "1" + "3";
+let sum_p = document.getElementById("sum");
+sum_p.innerHTML = sum;

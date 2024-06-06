03e09a31eaaa1f2c926af3391175185675126194
angel-721 <angelvelasquez2002@gmail.com>
2024-06-06 01:57:11 -0600

    gpa calc

---
+++ backend/week-1/day4/gpa-calc/app.js
@@ -0,0 +1,37 @@
+let gpa_points = 0;
+let get_gpa_button = document.getElementById("get-gpa");
+let gpa_h3 = document.getElementById("gpa");
+
+// TODO:
+// Make this all of the input elements
+// Make this a single line of code.
+// Hint: get all of the document.query....All(".class-name")
+// Hint: Please don't put my hint code exactly and expect it to work :)
+let inputs; // finish the rest
+
+let getGPA = () => {
+  // step 1 set some variable for gpa points and make it equal to 0 to start
+  // step 2 do the same but now for the gpa
+  // step 3 loop through inputs:{
+  // 		if a input value in inputs >= 90: gpaPoints += 4
+  // 		elif a input value in inputs >= 80: gpaPoints += 3
+  // 		elif a input value in inputs >= 70: gpaPoints += 2
+  // 		elif a input value in inputs >= 60: gpaPoints += 1
+  // 		else gpaPoints +=0
+  // }
+  //
+  // step 4 - after the loop now calculate the gpa
+  // HINT This is gpaPoints diveded by the number of classes(4 in this case)
+  // return it
+  //
+};
+
+get_gpa_button.onclick = () => {
+  // if any of the inputs have a value that's empty{
+  // 		return out and don't do anything :o
+  // }
+  // get the gpa using getGPA
+  // set the h3 text to be the gpa
+  //
+  // if gpa > 3.0 add the class of good-gpa to it
+};

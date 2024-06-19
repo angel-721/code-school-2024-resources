2c842d2b9fbeabf02295643b70f47182663bc471
angel-721 <angelvelasquez2002@gmail.com>
2024-06-19 11:35:12 -0600

    Update

--- backend/week-4/day-2/mongo/index.js
+++ backend/week-4/day-2/mongo/index.js
@@ -17,17 +17,44 @@ app.post("/students", function (request, response) {
   console.log(request.body);
   const data = request.body;
 
-  let newStudent = {
+  // create a new MongoStudent using our model
+  let newStudent = new model.Student({
     name: data.name,
     major: data.major,
-  };
+  });
 
-  students.push(newStudent);
+  newStudent
+    .save()
+    .then(() => {
+      // if the new Student saved right
+      response.setHeader("Access-Control-Allow-Origin", "*");
+      response.status(201).send("Created");
+    })
+    .catch(() => {
+      // if something goes wrong when saving a student
+      response.status(400).send("Something failed when making a student");
+    });
+});
 
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.status(201).send("Created Student");
+app.delete("/students/:id", (req, res) => {
+  console.log("DELETE for single student");
+  console.log(req.params.id);
+  model.Student.findOneAndDelete({ _id: req.params.id })
+    .then((student) => {
+      if (student) {
+        res.status(200).send("Removed");
+      } else {
+        console.log("Student not found.");
+        res.status(404).send("Student not found.");
+      }
+    })
+    .catch(() => {
+      console.log("Bad request (GET by ID).");
+      res.status(400).send("Student not found.");
+    });
 });
 
 app.listen(8080, () => {
   console.log("Server is running on http://localhost:8080");
-})
+});
+
